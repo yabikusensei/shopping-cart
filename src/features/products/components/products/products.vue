f7a6edb24a663e2a64d6daf450fadf647cde1e9f
tecsup <tecsup@gmail.com>
2019-06-13 15:49:30 -0500

    it added changes

--- src/features/products/components/products/products.vue
+++ src/features/products/components/products/products.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="products">
+    count{{count}}{{name}}
+    <button v-on:click="increment">increment</button>
     <cod-search-bar></cod-search-bar>
     <div class="products__list">
       <card v-for="(item, index) in 12" v-bind:id="index" v-bind:key="index"></card>
@@ -9,19 +11,60 @@
 
 <script>
 import Card from "../card/card";
-import CodSearchBar from '../search-bar/search-bar'
+import CodSearchBar from "../search-bar/search-bar";
+import { mapState } from "vuex";
+import { mapGetters } from "vuex";
+import { mapActions } from "vuex";
+
+import store from "vuex";
 export default {
   name: "cod-products",
   components: {
-    'card': Card,
-    'cod-search-bar': CodSearchBar
+    card: Card,
+    "cod-search-bar": CodSearchBar
+  },
+  data() {
+    return {
+      info1: ""
+    };
+  },
+  computed: {
+    comInfo1() {
+      return this.info1;
+    },
+    ...mapState({
+      // arrow functions can make the code very succinct!
+      count: state => state.count,
+      name: state => state.name,
+      products: state => state.products
+    }),
+    ...mapGetters({
+      // map `this.doneCount` to `this.$store.getters.doneTodosCount`
+      getId: "getTodoById"
+    })
+  },
+  methods: {
+    increment() {
+      // this.$store.dispatch("sincrement", 2);
+      this.add(2)
+      //this.$store.commit('increment', 10)
+    },
+     ...mapActions({
+      add: 'increment' // map `this.add()` to `this.$store.dispatch('increment')`
+    })
+  },
+  mounted() {
+    // console.log('mounted!!!')
+    // console.log('store', this.$store.state.count)
+    // console.log("store", this.$store.getters.getTodoById(1));
+    // console.log("getId", this.getId(0));
+    console.log('products', this.products.shopping)
   }
 };
 </script>
 
 <style lang="scss" scoped>
 .products {
-
   &__list {
     display: flex;
     flex-wrap: wrap;
--- src/store.js
+++ src/store.js
@@ -2,19 +2,57 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
-
-export default new Vuex.Store({
+const products = {
   state: {
-    count: 0
+    shopping: [
+      0, 1, 2
+    ]
   },
   mutations: {
-    increment(state) {
-      state.count++
+    increment(state, n) {
+      state.shopping.push(n)
     }
   },
   actions: {
-    increment (context) {
-      context.commit('increment')
+    increment({ commit }, n) {
+      commit('increment', n)
     }
+  },
+  getters: {}
+}
+
+export default new Vuex.Store({
+  state: {
+    count: 0,
+    name: 'sebas',
+    users: [
+      {
+        id: 0,
+        name: 'cero'
+      },
+      {
+        id: 1,
+        name: 'uno'
+      }
+    ]
+  },
+  getters: {
+    getTodoById: (state) => (id) => {
+      return state.users.find(user => user.id === id)
+    }
+  },
+  mutations: {
+    increment(state, n) {
+      state.count += n
+    }
+  },
+  actions: {
+    increment({ commit }, n) {
+      console.log('n', n)
+      commit('increment', n)
+    }
+  },
+  modules: {
+    products
   }
 })
